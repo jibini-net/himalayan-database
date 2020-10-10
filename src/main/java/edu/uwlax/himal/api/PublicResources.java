416b1c8832a22727212877e559b6266c0b11762a
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2020-10-09 21:07:05 -0500

    Basic equals/contains filters

--- src/main/java/edu/uwlax/himal/api/PublicResources.java
+++ src/main/java/edu/uwlax/himal/api/PublicResources.java
@@ -4,11 +4,14 @@ import edu.uwlax.himal.Himalayan;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Scanner;
 
 @Controller
 public class PublicResources
@@ -16,20 +19,62 @@ public class PublicResources
     @Autowired
     private Himalayan himalayan;
 
+    private boolean passes(String filter, JSONObject values)
+    {
+        Scanner scan = new Scanner(filter);
+
+        while (scan.hasNext())
+        {
+            String field = scan.next();
+            String operator = scan.next();
+            String arg = scan.next();
+
+            if (!values.has(field))
+                throw new IllegalStateException(String.format("Invalid filter field name '%s'", field));
+            String fieldValue = values.getString(field);
+
+            switch (operator)
+            {
+                case "equals":
+                    if (!fieldValue.equals(arg))
+                        return false;
+                    break;
+
+                case "contains":
+                    if (!fieldValue.toLowerCase().contains(arg.toLowerCase()))
+                        return false;
+                    break;
+
+                default:
+                    throw new IllegalStateException(String.format("Invalid filter operator '%s'", operator));
+            }
+        }
+
+        return true;
+    }
+
     @GetMapping("/member")
     @ResponseBody
-    public String mappedGetMembers()
+    public String mappedGetMembers(
+            @RequestParam(defaultValue = "") String filter
+    )
     {
         JSONArray array = new JSONArray();
+
         for (JSONObject object : himalayan.members.getContents())
-            array.put(object);
+        {
+            if (passes(filter, object))
+                array.put(object);
+        }
 
         return array.toString();
     }
 
     @GetMapping("/peak")
     @ResponseBody
-    public String mappedGetPeaks()
+    public String mappedGetPeaks(
+            @RequestParam(defaultValue = "") String filter
+    )
     {
         JSONArray array = new JSONArray();
 
@@ -48,7 +93,8 @@ public class PublicResources
                     object.put(key, knownCoords.get(key));
             }
 
-            array.put(object);
+            if (passes(filter, object))
+                array.put(object);
         }
 
         return array.toString();
@@ -56,11 +102,17 @@ public class PublicResources
 
     @GetMapping("/expedition")
     @ResponseBody
-    public String mappedGetExpeditions()
+    public String mappedGetExpeditions(
+            @RequestParam(defaultValue = "") String filter
+    )
     {
         JSONArray array = new JSONArray();
+
         for (JSONObject object : himalayan.expeditions.getContents())
-            array.put(object);
+        {
+            if (passes(filter, object))
+                array.put(object);
+        }
 
         return array.toString();
     }
